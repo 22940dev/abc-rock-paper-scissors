9ea1dbb12dc7a387056dacdaf9eccaf6bf71b082
josephden16 <tegajoseph2002@gmail.com>
2020-05-26 09:33:31 +0100

    worked on basic ui

---
+++ script.js
@@ -0,0 +1,11 @@
+let modal = document.querySelector(".modal");
+let rulesBtn = document.getElementById("rulesBtn");
+let closeButton = document.getElementById("icon-close");
+
+rulesBtn.addEventListener("click", ()=> {
+    modal.classList.toggle("show-modal")
+});
+
+closeButton.addEventListener("click", ()=> {
+    modal.classList.remove("show-modal");
+});
