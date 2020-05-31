1272546d07d68db665d7f4994545802598ff8399
josephden16 <tegajoseph2002@gmail.com>
2020-05-31 12:29:27 +0100

    idk

--- script.js
+++ script.js
@@ -18,39 +18,39 @@ closeButton.addEventListener("click", () => {
 // -----------------------------------
 //         G      A     M     E
 // -----------------------------------
-// const randomWeapon = () => {
-//     let i = Math.round(Math.random() * 2);
-//     houseChoice = weapons[i];
-//     return houseChoice;
-// }
 
-// const whoWins = (user, cpu) => {
-//     if (user === "rock" && cpu === "paper") {
-//         return "cpu";
-//     }
-//     else if (user === "rock" && cpu === "scissors") {
-//         return "user";
-//     }
-//     else if (user === "rock" && cpu === "rock") {
-//         return "draw";
-//     }
-//     else if (user === "scissors" && cpu === "rock") {
-//         return "cpu";
-//     }
-//     else if (user === "scissors" && cpu === "paper") {
-//         return "user";
-//     }
-//     else if (user === "scissors" && cpu === "scissors") {
-//         return "draw"
-//     }
-//     else if (user === "paper" && cpu === "scissors") {
-//         return "cpu"
-//     }
-//     else if (user === "paper" && cpu === "rock") {
-//         return "user"
-//     }
-//     else if (user === "paper" && cpu === "paper") {
-//         return "draw";
-//     }
-// }
+const randomWeapon = () => {
+    let i = Math.round(Math.random() * 2);
+    houseChoice = weapons[i];
+    return houseChoice;
+}
 
+const whoWins = (user, cpu) => {
+    if (user === "rock" && cpu === "paper") {
+        return "cpu";
+    }
+    else if (user === "rock" && cpu === "scissors") {
+        return "user";
+    }
+    else if (user === "rock" && cpu === "rock") {
+        return "draw";
+    }
+    else if (user === "scissors" && cpu === "rock") {
+        return "cpu";
+    }
+    else if (user === "scissors" && cpu === "paper") {
+        return "user";
+    }
+    else if (user === "scissors" && cpu === "scissors") {
+        return "draw"
+    }
+    else if (user === "paper" && cpu === "scissors") {
+        return "cpu"
+    }
+    else if (user === "paper" && cpu === "rock") {
+        return "user"
+    }
+    else if (user === "paper" && cpu === "paper") {
+        return "draw";
+    }
+}
