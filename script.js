7ffcea95de2f6a8347478f9823541f9e00fa02f6
josephden16 <tegajoseph2002@gmail.com>
2020-06-06 12:37:34 +0100

    worked on game logic and fixed some ui issues

--- script.js
+++ script.js
@@ -12,19 +12,131 @@ closeButton.addEventListener("click", () => {
 });
 
 
-
-
-
 // -----------------------------------
-//         G      A     M     E
+//         G     A     M     E
 // -----------------------------------
 
+// initializing game components
+let paper = document.querySelector("#paper-click");
+let scissors = document.querySelector("#scissors-click");
+let rock = document.querySelector("#rock-click");
+let weapons = ["rock", "paper", "scissors"];
+let choice;
+let houseChoice;
+let stepOne = document.querySelector(".step-1");
+let stepTwoThreeFour = document.querySelector(".step-2-3-4");
+let loseWin = document.querySelector(".lose-win");
+let displayWinnerTimeout;
+let outcome = document.querySelector(".outcome");
+let playAgainBtn = document.getElementById("playAgain");
+let socre = document.getElementById("score");
+let points = 0;
+
+// user components
+let wrapper = document.querySelector(".user-wrapper");
+let weapon = document.querySelector(".user-weapon");
+let userWeaponImg = document.querySelector(".userWeapon");
+
+// house components
+let houseWrapper = document.querySelector(".house-wrapper");
+let houseWeapon = document.querySelector(".house-weapon");
+let houseWeaponImg = document.querySelector(".houseWeapon");
+let houseTimeout;
+let unkown = document.querySelector(".unknown");
+
+// reset the game to step 1
+const resetGame = () => {
+    stepTwoThreeFour.style.display = "none";
+    loseWin.style.display = "none";
+    stepOne.style.display = "block";
+    unkown.style.display = "block";
+    houseWrapper.id = ``;
+    houseWeapon.id = ``;
+    houseWeaponImg.id = ``;
+    houseWeaponImg.src = ``;
+    playAgainBtn.style.color = "";
+}
+
+// event listeners
+paper.addEventListener("click", () => {
+    choice = "paper";
+    houseChoice = randomWeapon();
+    initUser(choice);
+    initHouse(houseChoice);
+    winner = whoWins(choice, houseChoice);
+    displayWinner(winner);
+});
+
+rock.addEventListener("click", () => {
+    choice = "rock";
+    houseChoice = randomWeapon();
+    initUser(choice);
+    initHouse(houseChoice);
+    winner = whoWins(choice, houseChoice);
+    displayWinner(winner);
+});
+
+scissors.addEventListener("click", () => {
+    choice = "scissors";
+    houseChoice = randomWeapon();
+    initUser(choice);
+    initHouse(houseChoice);
+    winner = whoWins(choice, houseChoice);
+    displayWinner(winner);
+});
+
+playAgainBtn.addEventListener("click", resetGame);
+
+// initializes the choice made by the user
+const initUser = (choice) => {
+    stepOne.style.display = "none";
+    stepTwoThreeFour.style.display = "flex";
+    wrapper.id = `${choice}-wrapper`;
+    weapon.id = `${choice}`;
+    userWeaponImg.id = `${choice}Img`;
+    userWeaponImg.src = `images/icon-${choice}.svg`;
+}
+
+// initializes the random choice made by the house
+const initHouse = (houseChoice) => {
+    houseTimeout = setTimeout(() => {
+        unkown.style.display = "none";
+        houseWrapper.id = `${houseChoice}-wrapper`;
+        houseWeapon.id = `${houseChoice}`;
+        houseWeaponImg.id = `${houseChoice}Img`;
+        houseWeaponImg.src = `images/icon-${houseChoice}.svg`;
+    }, 2000);
+}
+
+// function that displays the winner of the game
+const displayWinner = (winner) => {
+    displayWinnerTimeout = setTimeout(() => {
+        loseWin.style.display = "block";
+        if (winner === "user") {
+            outcome.textContent = "you win";
+            ++points;
+            socre.textContent = points;
+
+        } else if (winner == "cpu") {
+            outcome.textContent = "THE HOUSE WINS";
+            playAgainBtn.style.color = "red";
+            --points;
+            socre.textContent = points;
+        } else {
+            outcome.textContent = "IT'S A DRAW";
+        }
+    }, 2000);
+}
+
+
+// a function that randomly selects a weapon 
 const randomWeapon = () => {
     let i = Math.round(Math.random() * 2);
     houseChoice = weapons[i];
     return houseChoice;
 }
 
+// function that decides outcome of the battle
 const whoWins = (user, cpu) => {
     if (user === "rock" && cpu === "paper") {
         return "cpu";
