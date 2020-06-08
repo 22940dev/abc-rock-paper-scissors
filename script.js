68940b098563737f03ef9d70e85aaf3417960d08
Denedo Oghenetega Joseph <44759296+josephden16@users.noreply.github.com>
2020-06-08 12:21:20 +0100

    Update script.js
    improved random function

--- script.js
+++ script.js
@@ -127,11 +127,17 @@ const displayWinner = (winner) => {
         }
     }, 2000);
 }
-
+// function that generates random numbers
+// special thanks to Josh.
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min+1)) + min;
+}
 
 // a function that randomly selects a weapon 
 const randomWeapon = () => {
-    let i = Math.round(Math.random() * 2);
+    i = getRandomIntInclusive(0,2);
     houseChoice = weapons[i];
     return houseChoice;
 }
